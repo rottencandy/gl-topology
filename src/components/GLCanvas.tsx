26c3d1b4db2204d7b7060f657da92fc4a15bc254
Mohammed Saud <md.saud020@gmail.com>
2023-12-13 23:07:36 +0530

    use resize observer for canvas

--- src/components/GLCanvas.tsx
+++ src/components/GLCanvas.tsx
@@ -5,19 +5,21 @@ import { alignCam, repackObjects, startECSPipeline, StopECSPipeline } from '../u
 
 import './GLCanvas.scss';
 
-const prepareCanvas = (canvas: HTMLCanvasElement) => {
+
+const prepareCanvas = (canvas: HTMLCanvasElement, gl: WebGL2RenderingContext) => {
     const onDisplayResize = () => {
         const displayWidth = canvas.clientWidth;
         const displayHeight = canvas.clientHeight;
         if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
             canvas.width = displayWidth;
             canvas.height = displayHeight;
+            gl.viewport(0, 0, displayWidth, displayHeight);
+            alignCam();
         }
-        alignCam();
     };
 
-    canvas.onresize = onDisplayResize;
-
+    const observer = new ResizeObserver(onDisplayResize);
+    observer.observe(canvas);
     canvas.onpointermove = onPointerMove;
     canvas.onpointerup = onPointerUp;
     canvas.onpointerdown = onPointerDown;
@@ -29,7 +31,7 @@ export const GLCanvas: React.FC<{ res: K8sResourceCommon[] }> = ({ res }) => {
 
     React.useEffect(() => {
         const gl = canvasRef.current.getContext('webgl2');
-        prepareCanvas(canvasRef.current);
+        prepareCanvas(canvasRef.current, gl);
         startECSPipeline(gl);
 
         return () => {
